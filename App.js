1b5dc834795059375aab79df3d8664b536295bae
Cecile-Hirschauer <cecile.hirschauer@bbox.fr>
2023-03-17 15:22:18 +0100

    second commit

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useEffect, useState} from "react";
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const url = "https://jsonplaceholder.typicode.com/posts"
+
+  useEffect(() => {
+    fetch(url)
+        .then((resp) => resp.json())
+        .then((json) => setData(json))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+  }, []);
+
+  useEffect(() => {
+    console.log(data)
+  }, [data]);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        {loading ? (
+            <Text>Loading...</Text>
+        ) : (
+            data.map((post, index) => {
+              return (
+                  <View key={index}>
+                    <Text style={styles.title}>{post.title}</Text>
+                    <Text>{post.body}</Text>
+                  </View>
+              );
+            })
+        )}
+      </View>
   );
 }
 
